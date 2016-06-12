56247d5e5882a3e69ae23f03e68ea16e02e68efc
ksrubka <beata.choluj@wp.pl>
2016-06-12 16:59:27 +0200

    Implement methods

--- src/main/java/pl/com/bottega/documentmanagement/api/DocumentFlowProcess.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/DocumentFlowProcess.java
@@ -1,6 +1,9 @@
 package pl.com.bottega.documentmanagement.api;
 
+import pl.com.bottega.documentmanagement.domain.Document;
 import pl.com.bottega.documentmanagement.domain.DocumentNumber;
+import pl.com.bottega.documentmanagement.domain.DocumentNumberGenerator;
+import pl.com.bottega.documentmanagement.infrastructure.DocumentRepository;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -9,25 +12,40 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public class DocumentFlowProcess {
 
+    private DocumentNumberGenerator documentNumberGenerator;
+    private DocumentRepository documentRepository;
+    private UserManager userManager;
+
     public DocumentNumber create(String title, String content) {
         //todo walidacja długości i innych właściwości parametrów
         checkNotNull(title);
         checkNotNull(content);
-        return null;
+        DocumentNumber docNr = documentNumberGenerator.generate();
+        Document document = new Document(docNr, title, content);
+        documentRepository.save(document);
+        return docNr;
     }
 
     public void change(DocumentNumber docNr, String newTitle, String newContent) {
         checkNotNull(docNr);
         checkNotNull(newTitle);
         checkNotNull(newContent);
+        Document document = documentRepository.load(docNr);
+        document.change(newTitle, newContent);
+        documentRepository.save(document);
     }
 
     public void verify(DocumentNumber docNr) {
         checkNotNull(docNr);
+        Document document = documentRepository.load(docNr);
+        documentRepository.save(document);
     }
 
     public void publish(DocumentNumber docNr) {
         checkNotNull(docNr);
+        Document document = documentRepository.load(docNr);
+        document.verify(userManager.currentEmployee());
+        documentRepository.save(document);
     }
 
     public void archive(DocumentNumber docNr) {
--- src/main/java/pl/com/bottega/documentmanagement/api/DocumentsCatalog.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/DocumentsCatalog.java
@@ -1,7 +1,11 @@
 package pl.com.bottega.documentmanagement.api;
 
+import com.google.common.collect.Iterables;
 import pl.com.bottega.documentmanagement.domain.Document;
+import pl.com.bottega.documentmanagement.domain.DocumentCriteria;
 import pl.com.bottega.documentmanagement.domain.DocumentNumber;
+import pl.com.bottega.documentmanagement.infrastructure.DocumentDto;
+import pl.com.bottega.documentmanagement.infrastructure.DocumentRepository;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -10,13 +14,28 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public class DocumentsCatalog {
 
-    public Document get(DocumentNumber docNr) {
+    private DocumentRepository documentRepository;
+
+    public DocumentDto get(DocumentNumber docNr) {
         checkNotNull(docNr);
-        return null;
+        Document document = documentRepository.load(docNr);
+        return document.export();
     }
 
-    public Iterable<Document> find(DocumentCriteria documentCriteria) {
+    public Iterable<DocumentDto> find(DocumentCriteria documentCriteria) {
         checkNotNull(documentCriteria);
-        return null;
+        Iterable<Document> documents = documentRepository.find(documentCriteria);
+        // słaba wersja bez lambdy
+        /*Collection<DocumentDto> returnValue = new ArrayList<>();
+        for (Document document : documents)
+            returnValue.add(document.export());
+        return returnValue;*/
+
+        //lepsza wersja z lambdą
+        //return Iterables.transform(documents, (document) -> document.export());
+
+        //jeszcze lepsza wersja - ale wszystkie działają tak samo w sensie wyniku
+        return Iterables.transform(documents, Document::export);
+
     }
 }
