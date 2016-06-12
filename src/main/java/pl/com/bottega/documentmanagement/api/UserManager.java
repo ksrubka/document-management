966abcd7ee2185ef9e62552133af98452da1481f
ksrubka <beata.choluj@wp.pl>
2016-06-12 17:00:09 +0200

    Create UserManager class

---
+++ src/main/java/pl/com/bottega/documentmanagement/api/UserManager.java
@@ -0,0 +1,16 @@
+package pl.com.bottega.documentmanagement.api;
+
+import pl.com.bottega.documentmanagement.domain.Employee;
+
+/**
+ * Created by Beata Iłowiecka on 12.06.2016.
+ */
+public class UserManager {
+
+    public Employee currentEmployee() {
+        return null;
+    }
+
+    // metoda logIn - ustala czy pracownik jest zalogowany
+
+}
