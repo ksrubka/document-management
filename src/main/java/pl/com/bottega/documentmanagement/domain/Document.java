4acd806a2c426f051e74b9edc950dac69c62339c
ksrubka <beata.choluj@wp.pl>
2016-06-12 17:00:56 +0200

    Create methods

--- src/main/java/pl/com/bottega/documentmanagement/domain/Document.java
+++ src/main/java/pl/com/bottega/documentmanagement/domain/Document.java
@@ -1,7 +1,27 @@
 package pl.com.bottega.documentmanagement.domain;
 
+import pl.com.bottega.documentmanagement.infrastructure.DocumentDto;
+
 /**
  * Created by Beata Iłowiecka on 12.06.2016.
  */
 public class Document {
+
+    public Document(DocumentNumber docNr, String title, String content) {
+
+    }
+
+    public void change(String newTitle, String newContent) {
+
+    }
+
+    public void verify(Employee employee) {
+
+    }
+
+    public DocumentDto export() {
+        DocumentDto dto = new DocumentDto();
+        //tu przepakowanie seterami i geterami pól document na dto
+        return dto;
+    }
 }
