a60a2350c4ce3ff13c967c2b272ea76c0ae00cf8
ksrubka <beata.choluj@wp.pl>
2016-07-03 10:59:20 +0200

    Zajęcia 03-07

--- src/main/java/pl/com/bottega/documentmanagement/domain/Document.java
+++ src/main/java/pl/com/bottega/documentmanagement/domain/Document.java
@@ -2,13 +2,33 @@ package pl.com.bottega.documentmanagement.domain;
 
 import pl.com.bottega.documentmanagement.api.DocumentDto;
 
+import javax.persistence.*;
+
 /**
  * Created by maciuch on 12.06.16.
  */
+@Entity
 public class Document {
 
-    public Document(DocumentNumber documentNumber, String content, String title) {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Embedded
+    private DocumentNumber documentNumber;
+    private String content;
+    private String title;
 
+    @ManyToOne
+    private Employee creator;
+
+    public Document() {
+    }
+
+    public Document(DocumentNumber documentNumber, String content, String title, Employee creator) {
+        this.documentNumber = documentNumber;
+        this.content = content;
+        this.title = title;
+        this.creator = creator;
     }
 
     public void change(String title, String content) {
--- src/main/java/pl/com/bottega/documentmanagement/domain/DocumentNumber.java
+++ src/main/java/pl/com/bottega/documentmanagement/domain/DocumentNumber.java
@@ -1,8 +1,11 @@
 package pl.com.bottega.documentmanagement.domain;
 
+import javax.persistence.Embeddable;
+
 /**
  * Created by maciuch on 12.06.16.
  */
+@Embeddable
 public class DocumentNumber {
 
     private String number;
